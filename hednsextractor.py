b1793eddc3e290a2c9c974a9655247ef0c022376
unload <50772593+teixeira0xfffff@users.noreply.github.com>
2023-07-23 18:01:40 -0300

    Update hednsextractor.py
    update selenium config to randomize UA

--- hednsextractor.py
+++ hednsextractor.py
@@ -1,42 +1,26 @@
 import re
 import argparse
 import json
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
+import requests
 from bs4 import BeautifulSoup
+from random import randint
+from time import sleep
+from fake_useragent import UserAgent
 
-
-def get_page_response_with_selenium(url):
-    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--window-size=1920x1080")
-    chrome_options.add_argument(f"user-agent={user_agent}")
-    chrome_options.add_argument("--enable-javascript")
-    chrome_options.add_argument("--enable-cookies")
-
-    driver = webdriver.Chrome(options=chrome_options)
-
-    driver.get(url)
-    WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "div#netinfo"))
-    )
-
-    page_source = driver.page_source
-
-    driver.quit()
-
-    return page_source
+def get_page_response_with_requests(url):
+    ua = UserAgent()
+    headers = {
+        "User-Agent": ua.random,
+    }
+    session = requests.Session()
+    session.headers.update(headers)
+    response = session.get(url)
+    response.raise_for_status()
+    return response.text
 
 def extract_domains_from_html(html_content):
     domains = []
-
-    soup = BeautifulSoup(html_content, 'html.parser')
+    soup = BeautifulSoup(html_content, 'lxml')
     dns_links = soup.select('a[href^="/dns/"]')
 
     for link in dns_links:
@@ -48,21 +32,26 @@ def extract_domains_from_html(html_content):
     return domains
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Get the page source using Selenium.")
+    parser = argparse.ArgumentParser(description="Get the page source using requests with cookies, JS support, and random User-Agent.")
     parser.add_argument("target_url", help="The target URL.")
     parser.add_argument("--output-format", choices=["text", "json"], default="text", help="Output format (default: text)")
     args = parser.parse_args()
 
     target_url = args.target_url
-    page_response = get_page_response_with_selenium(target_url)
+
+    try:
+        page_response = get_page_response_with_requests(target_url)
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        exit(1)
 
     domains = extract_domains_from_html(page_response)
 
     if args.output_format == "json":
-        # Output as JSON
         domains_json = json.dumps(domains, indent=2)
         print(domains_json)
     else:
-        # Output as plain text
         for domain in domains:
             print(domain)
+
+    sleep(randint(1, 3))
