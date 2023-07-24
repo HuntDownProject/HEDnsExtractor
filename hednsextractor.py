b7729cd751ccd250e8208aa328f404dd73ee589d
unload <50772593+teixeira0xfffff@users.noreply.github.com>
2023-07-24 09:31:18 -0300

    update develp

--- hednsextractor.py
+++ hednsextractor.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 from random import randint
 from time import sleep
 from fake_useragent import UserAgent
+from datetime import datetime
 
 CONFIG_FILE = "config.json"
 
@@ -38,6 +39,13 @@ def extract_domains_from_html(html_content):
 
     return domains
 
+def convert_timestamp_to_date(timestamp):
+    try:
+        timestamp_int = int(timestamp)
+        return datetime.utcfromtimestamp(timestamp_int).strftime('%Y-%m-%d %H:%M:%S')
+    except (ValueError, TypeError):
+        return "Not available"
+
 def consult_vt(domain, vt_api_key):
     url = f"https://www.virustotal.com/api/v3/domains/{domain}"
     headers = {
@@ -49,17 +57,21 @@ def consult_vt(domain, vt_api_key):
         vt_data = response.json()
         attributes = vt_data.get("data", {}).get("attributes", {})
         last_analysis_stats = attributes.get("last_analysis_stats")
+        category = attributes.get("categories", [])
+        last_analysis_date = attributes.get("last_analysis_date")
+        
         if last_analysis_stats:
             malicious = last_analysis_stats.get("malicious", 0)
             suspicious = last_analysis_stats.get("suspicious", 0)
-            return int(malicious) + int(suspicious)
-    return None
+            last_analysis_date_str = convert_timestamp_to_date(last_analysis_date)
+            return int(malicious) + int(suspicious), category, last_analysis_date_str
+    return None, category, "Not available"
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Get the page source using requests with cookies, JS support, and random User-Agent.")
     parser.add_argument("target_url", help="The target URL.")
     parser.add_argument("--output-format", choices=["text", "json"], default="text", help="Output format (default: text)")
-    parser.add_argument("--consult-vt", action="score_true", help="Query VirusTotal for domain scores.")
+    parser.add_argument("--consult-vt", action="store_true", help="Query VirusTotal for domain scores.")
     args = parser.parse_args()
 
     target_url = args.target_url
@@ -77,9 +89,9 @@ if __name__ == "__main__":
 
     if args.consult_vt and vt_api_key:
         for domain in domains:
-            score = consult_vt(domain, vt_api_key)
+            score, categories, last_analysis_date = consult_vt(domain, vt_api_key)
             if score is not None:
-                print(f"Domain: {domain}, VT Score: {score}")
+                print(f"Domain: {domain}, VT Score: {score}, Last Analysis Date: {last_analysis_date}")
             else:
                 print(f"Domain: {domain}, VT Score: Not available (VT API key may be missing)")
 
